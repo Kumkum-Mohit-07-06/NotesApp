9c9204b0c556b6766345382b4f72442f82f23a19
KumkumMohit <sureshkumarsoni77913@gmail.com>
2025-06-23 01:04:25 -0700

    changes in frontend

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173', // <- Exact origin of your React app
+    origin: 'https://notesapp-frontend-dayp.onrender.com', // <- Exact origin of your React app
     credentials: true                // <- Allow cookies to be sent
 }));
 app.use(cookieParser())
